f51ef70e29ff32704d2ca411aeb88c67a256d1af
Simcha <Simchav0@gmail.com>
2024-10-31 17:37:07 +0200

    app/gql/input_types.py

--- app/gql/input_types.py
+++ app/gql/input_types.py
@@ -1,5 +1,6 @@
 from graphene import InputObjectType, Int, Float, String, Date
 
+
 class CreateMissionInput(InputObjectType):
     mission_date = Date(required=True)
     airborne_aircraft = Float(required=True)
@@ -10,12 +11,22 @@ class CreateMissionInput(InputObjectType):
     aircraft_damaged = Float()
     aircraft_lost = Float()
 
+
 class CreateTargetInput(InputObjectType):
     mission_id = Int(required=True)
     target_industry = String(required=True)
     city_id = Int(required=True)
-    target_type_id = Int()
+    target_type_id = Int(required=True)
     target_priority = Int()
+
+
+class CreateAttackResultsInput(InputObjectType):
+    mission_id = Int(required=True)
+    aircraft_returned = Float(required=True)
+    aircraft_failed = Float(required=True)
+    aircraft_damaged = Float(required=True)
+    aircraft_lost = Float(required=True)
+
 
 class UpdateAttackResultsInput(InputObjectType):
     aircraft_returned = Float()
---
+++ app/gql/types/__init__.py
@@ -0,0 +1,5 @@
+from .city_type import CityType
+from .country_type import CountryType
+from .mission_type import MissionType
+from .target_type import TargetType
+from .target_type_type import TargetTypeType
